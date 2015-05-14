35f6e338556637615f14f1bb4c3c92467db5c5c5
Kaoji <eliosjios@hotmail.com>
2015-05-14 15:31:26 +0200

    Youhou

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Lydia Jory & Adrien Morisse & Alexis Souchet" />
+    
+    <title>Vidya</title>
+    
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:700,400,300' rel='stylesheet' type='text/css'>
+    
+    <script type="application/javascript" src="http://code.jquery.com/jquery-1.11.2.js"></script>
+    
+
+</head>
+
+<body>
+
+<!-- HEADER //-->
+<header>
+    <div id="logo"></div>
+    <nav></nav>
+</header>
+    
+<!-- CONTENU //-->
+<main>
+    
+    <ul>
+        
+    <!-- ACCROCHE //-->
+    <li>
+    </li>
+        
+    <!-- CONCEPT //-->
+    <li>
+    </li>
+        
+    <!-- ESPACES //-->
+    <li>
+    </li>
+        
+    <!-- OUTILS //-->
+    <li>
+    </li> 
+        
+    <!-- APPLICATION //-->
+    <li>
+    </li>
+        
+    <!-- A PROPOS //-->
+    <li>
+    </li>
+        
+    </ul>
+    
+</main>
+    
+
+</body>
+
+</html>
