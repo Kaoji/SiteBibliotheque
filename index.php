5a126da49aff8714b44439e77afe7c59f4882e2a
Kaoji <eliosjios@hotmail.com>
2015-05-19 17:05:49 +0200

    MAJ Adrien

--- index.php
+++ index.php
@@ -22,7 +22,14 @@
 <!-- HEADER //-->
 <header>
     <div id="logo"></div>
-    <nav></nav>
+    <nav>
+        <ul>
+        <li>Le concept</li>
+        <li>Les espaces</li>
+        <li>L'application</li>
+        <li>A propos</li>
+        </ul>
+    </nav>
 </header>
     
 <!-- CONTENU //-->
@@ -32,31 +39,143 @@
         
     <!-- ACCROCHE //-->
     <li>
+        <article id="accroche">
+            <h1>Concepts innovants pour les bibliothèques</h1>
+            <video></video>
+        </article>
     </li>
         
     <!-- CONCEPT //-->
     <li>
+        <article>
+        <h2>Le concept</h2>
+        
+        <div id="slide">
+            
+            <article class="conceptSlide">
+                <h3>Avant, Pendant, Après</h3>
+                <p>Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</p>
+            </article>
+            <article class="conceptSlide">
+                <h3>Des espaces plus riche</h3>
+                <p>Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</p>
+            </article>
+            <article class="conceptSlide">
+                <h3>Une application pour noyau</h3>
+                <p>Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</p>
+            </article>
+        
+        </div>
+            
+        <div id="navSlide">
+            <a href="" class="rondSlide"></a>
+            <a href="" class="rondSlide"></a>
+            <a href="" class="rondSlide"></a>
+        </div>
+            
+        </article>
+        
     </li>
         
     <!-- ESPACES //-->
     <li>
-    </li>
+        <article>
+        <h2>Les espaces</h2>
         
-    <!-- OUTILS //-->
-    <li>
-    </li> 
+            <article>
+                <h3>Espace de collaboration</h3>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+                <h4>Outils à disposition</h4>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+                <h4>Caractéristiques</h4>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+            </article>
+
+            <article>
+                <h3>Espace d'immersion</h3>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+                <h4>Outils à votre disposition</h4>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+                <h4>Caractéristiques</h4>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+            </article>
+
+            <article>
+                <h3>Espace de détente</h3>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+                <h4>Outils à votre disposition</h4>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+                <h4>Caractéristiques</h4>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+            </article>
+            
+        </article>
+        
+    </li>
         
     <!-- APPLICATION //-->
     <li>
+        <article>
+        <h2>L'application</h2>
+        
+        <div>
+            
+            <article>
+                <h3>Chez vous, à la bibliothèque, à l'extérieur, au travail, chez un ami, ...</h3>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+            </article>
+
+            <article>
+                <h3>Restez en contact</h3>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+            </article>
+
+            <article>
+                <h3>Chez vous, à la bibliothèque, à l'extérieur, au travail, chez un ami, ...</h3>
+                <p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>
+            </article>
+        
+        </div>
+            
+        </article>
+        
     </li>
         
     <!-- A PROPOS //-->
     <li>
+        <article>
+        <h2>Qui sommes-nous</h2>
+        
+        <p></p>
+        
+        <ul>
+            <li>
+            <h4>Adrien Morisse</h4>
+            <p></p>
+            </li>
+            
+            <li>
+            <h4>Alexis Souchet</h4>
+            <p></p>
+            </li>
+            
+            <li>
+            <h4>Lydia Jory</h4>
+            <p></p>
+            </li>
+            
+        </ul>
+        
+        </article>
+        
     </li>
         
     </ul>
     
 </main>
+    
+<footer>
+</footer>
     
 
 </body>
